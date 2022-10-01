1133d9f1234ec406573ec6bc456c03d028472101
Pavel <kurilo.pavel@mail.ru>
2022-10-02 01:46:25 +0300

    fix: fixed mistake in Birthday

--- src/Birthday.js
+++ src/Birthday.js
@@ -1,15 +1,18 @@
 const Birthday = () => {
-  const date = new Date;
-  const today = date.getFullYear() + '-' + (date.getUTCMonth() < 9 ? '0' : '') + (date.getUTCMonth() + 1) + '-' + date.getUTCDate()
+  const date = new Date();
+  const today = date.getFullYear() + '-' + (date.getUTCMonth() < 9 ? '0' : '') + (date.getUTCMonth() + 1) + '-' + (date.getUTCDate() < 10 ? '0' : '') + date.getUTCDate();
+  console.log(today)
   return (
     <div>
       <label style={{fontWeight: 'bold', margin: '5px 0', display: 'block'}}>When is your birthday?</label>
-      <input type={'date'} max={today}
+      <input type={'date'}
+             min='1900-01-01'
+             max={today}
              style={{
-        boxSizing: 'border-box', width: '100%', padding: '0.5em',
-        border: '1px solid blue',
-        borderRadius: '0.2em'
-      }}/>
+               boxSizing: 'border-box', width: '100%', padding: '0.5em',
+               border: '1px solid blue',
+               borderRadius: '0.2em'
+             }}/>
       <p>We will not show your birthday to other users.</p>
     </div>
   )
