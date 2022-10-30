c25db9a53956c920916438a975ce22d0625c18d5
Igor <tanovigor@gmail.com>
2022-10-30 16:09:31 +0300

    rework logIn and reg display

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import Body from "./components/body/Body";
-import LoginPage from './pages/LoginPage'
-import FirstRegistrationForm from "./components/FirstRegistrationForm";
 
 function App(): any {
   return (
     <BrowserRouter>
       <div className="App">
         <Body />
-        <Routes>
-        <Route path='/users/sign_in' element={<LoginPage/>}/>
-        <Route path='/users/sign_up' element={<FirstRegistrationForm/>}/>
-      </Routes>
       </div>
     </BrowserRouter>
   );
