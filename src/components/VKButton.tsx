833b67d5819b6f1fe32edc582d59cf75e7c20eb4
Nalallia <mnatasha11@mail.ru>
2022-10-31 22:36:38 +0300

    minor code improvements

--- src/components/VKButton.tsx
+++ src/components/VKButton.tsx
@@ -4,9 +4,13 @@ import { REDIRECT_URL, VK_APP, BACKEND_URL } from "../constants";
 const VKButton = () => {
   const handleRedirect = () => {
     window.location.href = `https://oauth.vk.com/authorize?client_id=${VK_APP.id}&display=popup&redirect_uri=${REDIRECT_URL}&scope=email&response_type=code&v=5.120&state=4194308`;
-    const code = getVKCode(window.location.href)!;
-    if (code) console.log(code);
-    else if (code === null) window.location.href = BACKEND_URL;
+    const code = getVKCode(window.location.href);
+    if (code) {
+      console.log(code);
+    }
+    else {
+      window.location.href = BACKEND_URL;
+    }
   };
 
   return (
