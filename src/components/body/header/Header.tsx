2565d50e653485be5d1a8342d6615a3c96899bef
Igor <tanovigor@gmail.com>
2022-11-01 19:42:36 +0300

    2 button

--- src/components/body/header/Header.tsx
+++ src/components/body/header/Header.tsx
@@ -1,20 +1,36 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import { useTheme } from "../../../hooks/useTheme/useTheme";
 
 const Header = () => {
+  let { theme, setTheme } = useTheme();
+  const darkTheme = () => {
+    setTheme("dark");
+  };
+  const lightTheme = () => {
+    setTheme("light");
+  };
   return (
     <div className="nav-bar">
+      <div className="change-themes">
+      <button className="change_theme" onClick={darkTheme}>
+        Dark
+      </button>
+      <button className="change_theme" onClick={lightTheme}>
+        Light
+      </button>
+      </div>
       <Link to="/">
         <div>Lessoner</div>
       </Link>
       <Link to="/categories">
-        <div >Categories</div>
+        <div>Categories</div>
       </Link>
       <Link to="/lessons">
-        <div >Lessons</div>
+        <div>Lessons</div>
       </Link>
       <Link to="/about">
-        <div >About</div>
+        <div>About</div>
       </Link>
       <div>
         <button>English</button>
