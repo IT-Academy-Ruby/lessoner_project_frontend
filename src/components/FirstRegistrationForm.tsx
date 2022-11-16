bd049331b029c81fad6c723a57b588151a920d40
NatashaMikhaltsova <47752551+NatashaMikhaltsova@users.noreply.github.com>
2022-11-16 23:15:31 +0300

    added buttons for social media

--- src/components/FirstRegistrationForm.tsx
+++ src/components/FirstRegistrationForm.tsx
@@ -1,13 +1,18 @@
 /* eslint-disable max-len */
 import "./FirstRegistrationForm.scss";
 import {
-  Field, Form, Formik 
+  Field, Form, Formik
 } from "formik";
-import { FormattedMessage , useIntl } from "react-intl";
 import Checkbox from "./Checkbox";
 import Email from "./Email";
+import FacebookButton from "./FacebookButton";
+import { GOOGLE_APP } from "../constants";
+import GoogleButton from "./GoogleButton";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import PasswordAndConfirm from "./PasswordAndConfirm";
+import VKButton from "./VKButton";
 import { isEmailExists } from "../services/api/isEmailExists";
+import { useIntl } from "react-intl";
 
 interface FormValues {
   email: string;
@@ -33,7 +38,7 @@ const emailInvalidationRules = [
 
 const minSymbol = 6;
 const maxSymbol = 256;
-const allowPasswordSymbols= "! # $ % & ' * + - / = ? ^ _  { | } ~";
+const allowPasswordSymbols = "! # $ % & ' * + - / = ? ^ _  { | } ~";
 const passwordRegex = new RegExp("^[-/=!#$%&'*+?^_`{|}~.A-Z0-9]{" + minSymbol + "," + maxSymbol + "}$", "i");
 const FirstRegistrationForm = () => {
   const intl = useIntl();
@@ -57,10 +62,10 @@ const FirstRegistrationForm = () => {
     }
     if (!passwordRegex.test(values.password)) {
       errors.password = intl.formatMessage({ id: "app.firstRegistrationForm.passwordRegEx" }, {
-        minSymbol, maxSymbol, symbols: allowPasswordSymbols 
+        minSymbol, maxSymbol, symbols: allowPasswordSymbols
       });
     }
-    if (values.password.length >= maxSymbol || values.password.length < minSymbol) {
+    if (values.password.length > maxSymbol || values.password.length < minSymbol) {
       errors.password = intl.formatMessage({ id: "app.firstRegistrationForm.passwordLength" }, { minSymbol, maxSymbol });
     }
     if (values.password !== values.confirmPassword) {
@@ -86,15 +91,18 @@ const FirstRegistrationForm = () => {
       >
         {({ errors, touched }) => {
           return (
-            <Form className="First-Registration-Form">
-              <Field name='email' component={Email} error={touched.email ? errors.email : undefined} />
-              <Field name='password' component={PasswordAndConfirm} minSymbol={minSymbol} maxSymbol={maxSymbol} isConfirm={false} error={touched.password ? errors.password : undefined} />
-              <Field name='confirmPassword' component={PasswordAndConfirm} minSymbol={minSymbol} maxSymbol={maxSymbol} isConfirm={true} error={touched.confirmPassword ? errors.confirmPassword : undefined} />
-              <Field name='hasTermsAndConditions' component={Checkbox} error={touched.hasTermsAndConditions ? errors.hasTermsAndConditions : undefined} />
-              <button type="submit">
-                <FormattedMessage id="app.firstRegistrationForm.button" />
-              </button>
-            </Form>
+            <div className="first-registration-form">
+              <Form>
+                <Field name='email' component={Email} error={touched.email ? errors.email : undefined} />
+                <Field name='password' component={PasswordAndConfirm} minSymbol={minSymbol} maxSymbol={maxSymbol} isConfirm={false} error={touched.password ? errors.password : undefined} />
+                <Field name='confirmPassword' component={PasswordAndConfirm} minSymbol={minSymbol} maxSymbol={maxSymbol} isConfirm={true} error={touched.confirmPassword ? errors.confirmPassword : undefined} />
+                <Field name='hasTermsAndConditions' component={Checkbox} error={touched.hasTermsAndConditions ? errors.hasTermsAndConditions : undefined} />
+                <button className='registration-form-submit-button' type="submit">Next</button>
+              </Form>
+              <GoogleOAuthProvider clientId={GOOGLE_APP.id}><GoogleButton /></GoogleOAuthProvider>
+              <FacebookButton />
+              <VKButton />
+            </div>
           );
         }}
       </Formik>
