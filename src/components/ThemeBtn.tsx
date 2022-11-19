d886ba5fd0f2f95eb24337518e01ee2b8c3c1d44
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-11-19 15:45:15 +0300

    add theme

---
+++ src/components/ThemeBtn.tsx
@@ -0,0 +1,14 @@
+import "./ThemeBtn.scss";
+import useDarkMode from "use-dark-mode";
+import { useTheme } from "../utils/useTheme";
+
+const ThemeBtn = () => {
+  const darkMode = useDarkMode(true);
+  const theme = useTheme();
+  return (
+    <button className="btn-theme" type="button" onClick={darkMode.toggle}>
+      {theme === "dark-mode" ? "Light mode" : "Dark mode"}
+    </button>
+  );
+};
+export default ThemeBtn;
