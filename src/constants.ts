e59bc8617739de97f3410704bcc47b82c9a7661b
Kurilo-Pavel <75971336+Kurilo-Pavel@users.noreply.github.com>
2022-11-21 01:26:43 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -7,10 +7,10 @@ const DEFAULT_COUNTRY_CODE = "375";
 const FACEBOOK_APP = { id: "680825793391734" };
 const GOOGLE_APP =
   { id: "1055699025819-ebrdnj83l419b2hfj4j4l34joh83he25.apps.googleusercontent.com" };
-  const REDIRECT_URL = "https://lessoner.herokuapp.com/users/sign_up";
-  const VK_APP = { id: 51463254 };
+const REDIRECT_URL = "https://lessoner.herokuapp.com/users/sign_up";
+const VK_APP = { id: 51463254 };
  
  export {
   BACKEND_URL, EMAIL, PASSWORD, USERNAME, CODE, DEFAULT_COUNTRY_CODE,
-   FACEBOOK_APP, GOOGLE_APP, REDIRECT_URL, VK_APP
+  FACEBOOK_APP, GOOGLE_APP, REDIRECT_URL, VK_APP
 };
