74fab7f25ac71cd7aee1df8a8fd02abc1e223fb4
OxygenO22 <bamaik@mail.ru>
2022-11-23 20:51:42 +0300

    some fixes + added font

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -12,22 +12,20 @@ type CheckboxProps = {
 }
 const Checkbox = ({ field, error }: CheckboxProps): JSX.Element => {
   return (
-    <div className="checkbox__wrapper">
-      <label className="checkbox__label">
-        <div className="checkbox__content">
-          <input
-            className="checkbox__input"
-            type='checkbox'
-            {...field}
-          />
-          <p className="checkbox__text">
-            <FormattedMessage id="app.checkbox" /> <a href='#!'>
-              <FormattedMessage id="app.checkbox.terms" /> </a>
-          </p>
-        </div>
-        {error && <div><span className="error-message">{error}</span></div>}
-      </label>
-    </div>
+    <label className="checkbox__label">
+      <div className="checkbox__content">
+        <input
+          className="checkbox__input"
+          type='checkbox'
+          {...field}
+        />
+        <p className="checkbox__text">
+          <FormattedMessage id="app.checkbox" /> <a href='#!'>
+            <FormattedMessage id="app.checkbox.terms" /> </a>
+        </p>
+      </div>
+      {error && <div><span className="error-message">{error}</span></div>}
+    </label>
   );
 };
 
