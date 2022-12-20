5d93d9e3dbcbce2b76b1d1726038c5807e9fa86d
OxygenO22 <bamaik@mail.ru>
2022-12-20 16:54:07 +0300

    Add almost all svg

--- src/components/NavbarStudyStudio.tsx
+++ src/components/NavbarStudyStudio.tsx
@@ -13,41 +13,159 @@ const NavbarStudyStudio: FC<NavbarStudyStudioProps> = ({menuType}) => {
   const EMPTY_BUTTON_ID = 0;
   const [isMenuActive, setIsMenuActive] = useState(false);
   const [buttonPressed, setButtonPressed] = useState(EMPTY_BUTTON_ID);
-  const items = [ 
+  const items = [
     {
-      id: 1, value: "Menu", href: "#!", icon: "menu", place: "openclose"
-    },  
+      id: 1,
+      value: "Menu",
+      href: "#!",
+      icon: "menu",
+      place: "openclose",
+    },
     {
-      id: 2, value: "Close", href: "#!", icon: "close", place: "openclose"
-    }, 
+      id: 2,
+      value: "Close",
+      href: "#!",
+      icon: "close",
+      place: "openclose",
+    },
     {
-      id: 3, value: `${intl.formatMessage({ id: "app.navbarStudyStudio.home"})}`,
-      href: "#!", icon: "home", place: "study"
-    }, 
+      id: 3,
+      value: `${intl.formatMessage({ id: "app.navbarStudyStudio.home" })}`,
+      href: "#!",
+      icon: "home",
+      place: "study",
+    },
     {
-      id: 4, value: `${intl.formatMessage({ id: "app.navbarStudyStudio.categories"})}`, 
-      href: "#!", icon: "categories", place: "study"
-    }, 
+      id: 4,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.categories",
+      })}`,
+      href: "#!",
+      icon: "categories",
+      place: "study",
+    },
     {
-      id: 5, value: `${intl.formatMessage({ id: "app.navbarStudyStudio.subscription"})}`,
-      href: "#!", icon: "subscription", place: "study"
-    }, 
+      id: 5,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.subscription",
+      })}`,
+      href: "#!",
+      icon: "subscription",
+      place: "study",
+    },
     {
-      id: 6, value: `${intl.formatMessage({ id: "app.navbarStudyStudio.myLessons"})}`,
-      href: "#!", icon: "hat_mylesson", place: "study"
-    }, 
+      id: 6,
+      value: `${intl.formatMessage({ id: "app.navbarStudyStudio.myLessons" })}`,
+      href: "#!",
+      icon: "hat_mylesson",
+      place: "study",
+    },
     {
-      id: 7, value: `${intl.formatMessage({ id: "app.navbarStudyStudio.watchLater"})}`,
-      href: "#!", icon: "watch_later", place: "study"
-    }, 
+      id: 7,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.watchLater",
+      })}`,
+      href: "#!",
+      icon: "watch_later",
+      place: "study",
+    },
     {
-      id: 8, value: `${intl.formatMessage({ id: "app.navbarStudyStudio.myLesson"})}`,
-      href: "#!", icon: "my_lesson", place: "studio"
-    }, 
+      id: 8,
+      value: `${intl.formatMessage({ id: "app.navbarStudyStudio.myLesson" })}`,
+      href: "#!",
+      icon: "my_lesson",
+      place: "studio",
+    },
     {
-      id: 9, value: `${intl.formatMessage({ id: "app.navbarStudyStudio.management"})}`,
-      href: "#!", icon: "management", place: "studio"
-    }, 
+      id: 9,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "management",
+      place: "studio",
+    },
+    {
+      id: 10,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "about_us",
+      place: "study",
+    },
+    {
+      id: 11,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "edit_categories",
+      place: "study",
+    },
+    {
+      id: 12,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "log_in",
+      place: "study",
+    },
+    {
+      id: 13,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "log_out",
+      place: "study",
+    },
+    {
+      id: 14,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "darc_theme",
+      place: "study",
+    },
+    {
+      id: 15,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "light_theme",
+      place: "study",
+    },
+    {
+      id: 16,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "network",
+      place: "study",
+    },
+    {
+      id: 17,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "instagram",
+      place: "study",
+    },
+    {
+      id: 18,
+      value: `${intl.formatMessage({
+        id: "app.navbarStudyStudio.management",
+      })}`,
+      href: "#!",
+      icon: "facebook",
+      place: "study",
+    },
   ];
 
   const imageWrapperCN = classNames("image__wrapper", {"image__wrapper--active": !isMenuActive});
