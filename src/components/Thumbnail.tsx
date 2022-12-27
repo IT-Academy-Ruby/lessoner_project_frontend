f494fd42bcad2df08cb1976f71f892eea388a5eb
OxygenO22 <bamaik@mail.ru>
2022-12-27 13:46:00 +0300

    intermediate result 1

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,34 +1,36 @@
-import "./Thumbnail.scss"
-import React from "react";
-import classNames from "classnames";
+import "./Thumbnail.scss";
+import Button from "./Button";
 import frame85 from "./icons/Frame85.png";
 import { useIntl } from "react-intl";
-import Button from "./Button";
 
 export const Thumbnail = () => {
   const intl = useIntl();
   return (
-    <div className="evlth__inner">
-      <div className="evlth__inner-left">
-        <div className="evlth__item">
-          <img className="evlth__item-img" src={frame85} alt="picture" />
-          <div className="evlth__item-info">
-            <p className="evlth__item-name">Image-thumb.jpg</p>
-            <p className="evlth__item-size">2.3 MB</p>
+    <div className="thumbnail__wrapper">
+      <div className="thumbnail__left">
+        <div className="thumbnail__left-inner">
+          <div className="thumbnail__picture">
+            <img className="thumbnail__img" src={frame85} alt="picture" />
+          </div>
+          <div className="thumbnail__item-info">
+            <p className="thumbnail__item-name">Image-thumb.jpg</p>
+            <p className="thumbnail__item-size">2.3 MB</p>
           </div>
         </div>
       </div>
-      <div className="evlth__inner-right">
-        <Button
-          buttonType="button"
-          buttonText={intl.formatMessage({ id: "app.button.cancel" })}
-          className="button__fs16-white button__fs16-white-evlt"
-        />
-        <Button
-          buttonType="submit"
-          buttonText={intl.formatMessage({ id: "app.button.save" })}
-          className="button__fs16"
-        />
+      <div className="thumbnail__right">
+        <div className="thumbnail__btn-wrapper">
+          <Button
+            buttonType="button"
+            buttonText={intl.formatMessage({ id: "app.button.cancel" })}
+            className="button__fs16-white button__fs16-white-evlt"
+          />
+          <Button
+            buttonType="button"
+            buttonText={intl.formatMessage({ id: "app.button.save" })}
+            className="button__fs16"
+          />
+        </div>
       </div>
     </div>
   );
