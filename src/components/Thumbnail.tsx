231bf9b963a53a241e68b27e5b2ebe16e371a353
OxygenO22 <bamaik@mail.ru>
2022-12-28 21:36:22 +0300

    Try to upload image on server

---
+++ src/components/Thumbnail.tsx
@@ -0,0 +1,99 @@
+import "./Thumbnail.scss";
+import frame85 from "./icons/Frame85.png";
+import Placeholder from "./icons/PlaceholderM.png";
+import { useIntl } from "react-intl";
+import React, { ChangeEvent, FC, useState } from "react";
+import request from "../services/request";
+
+interface ThumbnailProps {
+  propImage?: string;
+}
+
+interface imageTypes {
+  lastModified?: number;
+  lastModifiedDate?: Date;
+  name: string;
+  size: number;
+  type?: string;
+  webkitRelativePath?: string;
+}
+
+export const Thumbnail: FC<ThumbnailProps> = (propImage) => {
+  const intl = useIntl();
+  const fileReader = new FileReader();
+  const [image, setImage] = useState<imageTypes>();
+  const [imageURL, setImageURL] = useState<any>();
+  fileReader.onloadend = () => {
+    setImageURL(fileReader.result);
+  }
+  const handleOnChange = (e: ChangeEvent<any>) => {
+    e.preventDefault();
+    console.log("change", e.target.files[0]);
+    if (e.target.files[0].size <= 2000000) {
+      const file = e.target.files[0];
+      setImage(file);
+      fileReader.readAsDataURL(file);
+
+
+    /* const formData = new FormData();
+    formData.append("file", file)
+    request("https://lessoner.s3.amazonaws.com/", "PUT", formData); */
+
+    } else {
+      alert("size to big: " + (Math.floor(e.target.files[0].size)/1000000).toFixed(2) )
+    }
+  };
+
+  console.log(propImage.propImage);
+
+  
+
+  
+
+  return (
+    <div className="thumbnail__wrapper">
+      <div className="thumbnail__left">
+        <div className="thumbnail__left-inner">
+          <div className="thumbnail__picture">
+            <img
+              className="thumbnail__img"
+              src={
+                imageURL
+                  ? imageURL
+                  : propImage.propImage != null
+                  ? propImage.propImage
+                  : frame85
+              }
+              alt="picture"
+            />
+          </div>
+          <div className="thumbnail__item-info">
+            <p
+              className="thumbnail__item-name"
+              title={image ? image.name : "Some.png"}
+            >
+              {image ? image.name : "Some image name.png"}
+            </p>
+            <p className="thumbnail__item-size">
+              {image ? (Math.floor(image.size) / 1000000).toFixed(2) : "0.0"} MB
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="thumbnail__right">
+        <div className="thumbnail__right-inner">
+          <label htmlFor="but__loader" className="button__fs16-white">
+            <div className="svg__change"></div>
+            {intl.formatMessage({ id: "app.button.change" })}
+          </label>
+          <input
+            id="but__loader"
+            type="file"
+            className="button__notsee"
+            onChange={handleOnChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/types/types.tsx
+++ src/components/types/types.tsx
@@ -1,11 +1,16 @@
 export interface ILessonBack {
   author_id: number;
   category_id: number;
+  created_at: string;
   description: string;
   id: number;
+  image_link?: string;
+  rating: number | null;
   status: string;
   title: string;
   video_link: string;
+  views_count: number;
+  votes_count: number;
 }
 
 export interface LessonCardsProps {
