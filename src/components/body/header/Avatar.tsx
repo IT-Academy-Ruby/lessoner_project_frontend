3b1efef343c66db71c8742f4d7243b829b79bc43
Sergey Dmitriev <terapool@gmail.com>
2023-01-17 15:10:22 +0300

    Fix click outside in popup (#121)
    Co-authored-by: valer-sky <89993618+valer-sky@users.noreply.github.com>

--- src/components/body/header/Avatar.tsx
+++ src/components/body/header/Avatar.tsx
@@ -4,6 +4,7 @@ import Language from "./Language";
 import {Link} from "react-router-dom";
 import Logout from "../../icons/logOut.svg";
 import {useAppSelector} from "../../../store/hooks";
+import useOnclickOutside from "react-cool-onclickoutside";
 import {useState} from "react";
 
 type AvatarProps = {
@@ -20,6 +21,9 @@ const Avatar = ({
   const nameDecode = useAppSelector(state => state.userDecodedName.session.name);
   const userAvatar = useAppSelector(state => state.dataUser.user.avatar_url);
 
+  const closeMenu = (): void => setIsChecked(false);
+  const ref = useOnclickOutside(() => closeMenu());
+
   const initialName = nameDecode.split(" ")
     .map(word => word[0]).slice(0, 2).join("").toLocaleUpperCase();
 
@@ -34,36 +38,38 @@ const Avatar = ({
         {!userAvatar && <p className="first-letters">{initialName}</p>}
         {userAvatar && <img src={userAvatar} alt="avatar" className="user-avatar" />}
       </label>
-      <input
-        type="checkbox"
-        className="input-lang"
-        id="input-avatar"
-        checked={isChecked}
-        onChange={() => {
-          setIsChecked(!isChecked);
-        }}
-      />
-      <ul className="list list-user">
-        <li className="user-li">
-          <Link to="/user/userPage" className="user-link" onClick={()=>setIsChecked(false)}>
-            <FormattedMessage id="app.avatar.personalData" />
-          </Link>
-        </li>
-        <li className="user-li">
-          <Language
-            onLanguageSwitch={onLanguageSwitch}
-            isRegistered={true}
-            language={language}
-            setLanguage={setLanguage}
-          />
-        </li>
-        <li className="user-li" onClick={signOut}>
-          <div className="log-out">
-            <FormattedMessage id="app.avatar.logOut" />
-            <img src={Logout} alt="logout" />
-          </div>
-        </li>
-      </ul>
+      <div className="clickoutside" ref={ref}>
+        <input
+          type="checkbox"
+          className="input-lang"
+          id="input-avatar"
+          checked={isChecked}
+          onChange={() => {
+            setIsChecked(!isChecked);
+          }}
+        />
+        <ul className="list list-user">
+          <li className="user-li">
+            <Link to="/user/userPage" className="user-link" onClick={()=>setIsChecked(false)}>
+              <FormattedMessage id="app.avatar.personalData" />
+            </Link>
+          </li>
+          <li className="user-li">
+            <Language
+              onLanguageSwitch={onLanguageSwitch}
+              isRegistered={true}
+              language={language}
+              setLanguage={setLanguage}
+            />
+          </li>
+          <li className="user-li" onClick={signOut}>
+            <div className="log-out">
+              <FormattedMessage id="app.avatar.logOut" />
+              <img src={Logout} alt="logout" />
+            </div>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
