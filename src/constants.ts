65920103d4523d69fda9df490e8149a4523b3078
Nalallia <mnatasha11@mail.ru>
2023-01-18 18:52:53 +0300

    Remove sensitive constants from the app

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,12 @@
-const BACKEND_URL = "https://lessoner-project-2w3h.onrender.com";
-const BACKEND_URL_LESSONS = BACKEND_URL + "/lessons/";
+const BACKEND_URL_LESSONS = process.env.REACT_APP_BACKEND_URL + "/lessons/";
 const CODE = {maxLength: 5};
 const DEFAULT_COUNTRY_CODE = "";
 const EMAIL = {minLength: 3, maxLength: 256};
-const FACEBOOK_APP = {id: "680825793391734"};
-const GOOGLE_APP =
-  {id: "1055699025819-ebrdnj83l419b2hfj4j4l34joh83he25.apps.googleusercontent.com"};
 const PASSWORD = {
   minLength: 6, maxLength: 256, symbols: "! # $ % & ' * + - / = ? ^ _  { | } ~"
 };
-const REDIRECT_URL = `${BACKEND_URL}/users/sign_up`;
+const REDIRECT_URL = `${process.env.REACT_APP_BACKEND_URL}/users/sign_up`;
 const USERNAME = {minLength: 3, maxLength: 50};
-const VK_APP = {id: 51463254};
 const maxNameLength = 64;
 const maxDescriptionLength = 600;
 const maxDescrHTCount = 10;
@@ -39,9 +34,8 @@ const VIDEO_DATA = {
 const SKELETON_LESSONS_AMOUT = 12;
 const AVATAR = {size: 5 * 1024 * 1024};
 
-export {
-  BACKEND_URL, BACKEND_URL_LESSONS, CODE, DEFAULT_COUNTRY_CODE, EMAIL, FACEBOOK_APP, GOOGLE_APP,
-  PASSWORD, REDIRECT_URL, USERNAME, VK_APP, maxNameLength, IMAGE_DATA, THUMBNAIL_DATA,
+export { BACKEND_URL_LESSONS, CODE, DEFAULT_COUNTRY_CODE, EMAIL,
+  PASSWORD, REDIRECT_URL, USERNAME, maxNameLength, IMAGE_DATA, THUMBNAIL_DATA,
   maxDescriptionLength, maxDescrHTCount, NAME_CATEGORY, DESCRIPTION_CATEGORY, SKELETON_AMOUT,
   NAVBAR_ADMIN, NAVBAR_AUTORISED, NAVBAR_NOT_AUTORISED, VIDEO_DATA, SKELETON_LESSONS_AMOUT,
   AVATAR
