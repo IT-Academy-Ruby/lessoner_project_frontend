49e226e36e62428585d3212111483cfeb41392b3
Igor <tanovigor@gmail.com>
2023-01-22 17:43:04 +0300

    terms fix

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,20 +1,23 @@
 import "./checkbox.scss";
-import Terms from "../pages/Terms";
-import {useState} from "react";
+import Terms from "../pages/terms/Terms";
+import { useState } from "react";
 
 type CheckboxProps = {
   field?: {
-    name: string,
-    onBlur: React.FocusEventHandler<HTMLInputElement>,
-    onChange: React.ChangeEventHandler<HTMLInputElement>,
-    value: string
+    name: string;
+    onBlur: React.FocusEventHandler<HTMLInputElement>;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    value: string;
   };
   information: string;
   link: string;
   error?: string;
-}
+};
 const Checkbox = ({
-  field, error, information, link
+  field,
+  error,
+  information,
+  link,
 }: CheckboxProps): JSX.Element => {
   const [isChecked, setIsChecked] = useState(false);
   const [isOpenTerms, setIsOpenTerms] = useState(false);
@@ -29,15 +32,18 @@ const Checkbox = ({
         className={isChecked ? "checked" : "unchecked"}
         {...field}
       />
-      <label
-        htmlFor="checkbox"
-        className="label-checkbox"
-      >
+      <label htmlFor="checkbox" className="label-checkbox">
         {information}
-        {link ? <span onClick={() => setIsOpenTerms(true)} className="link">{link}</span> : null}
+        {link ? (
+          <span onClick={() => setIsOpenTerms(true)} className="link">
+            {link}
+          </span>
+        ) : null}
         {error && <span className="error-message">{error}</span>}
       </label>
-      {isOpenTerms && <Terms setIsOpenTerms={setIsOpenTerms} isPolitic={false}/>}
+      {isOpenTerms && (
+        <Terms setIsOpenTerms={setIsOpenTerms} isPolitic={false} />
+      )}
     </span>
   );
 };
