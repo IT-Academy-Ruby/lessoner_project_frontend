a927c1252da0ce007c0c77bf14ba9350e85f6256
OxygenO22 <bamaik@mail.ru>
2023-01-25 12:46:28 +0300

    Hotfix to show noLessonPage

--- src/components/renderLessonsPage/renderLessonsContent.tsx
+++ src/components/renderLessonsPage/renderLessonsContent.tsx
@@ -262,12 +262,12 @@ export const RenderLessonContent: FC<RenderLessonContentProps> = (renderProps) =
   return (
     <div className={renderProps.classNameWrapper}>
       <div className={renderProps.classNameInner}>
-        {(renderProps.category && lessonsByCategory.length === 0) ||
-        (renderProps.categoryActive &&
-          lessonsBycategoryActive.length === 0 &&
-          renderProps.categoryActive !== "All categories") ? (
-            <NoLessonsPage isOnLessonsPage={false} />
-          ) : (
+        { (renderProps.category === undefined && lessonsByCategory.length === 0) 
+          || (renderProps.categoryActive === "") 
+          || (renderProps.categoryActive !== "All categories" 
+            && lessonsBycategoryActive.length === 0) 
+          ? (<NoLessonsPage isOnLessonsPage={false} />) 
+          : (
             getRenderByCurrentCategoryAndStatus().map((obj) => (
               <LessonCard
                 key={obj.id}
